3addc88b7a2017a0ae938dd106a9d53711c15be4
vedantk28 <vedant.22211498@viit.ac.in>
2025-10-01 16:16:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))  # Default to 8080 if PORT not set
+    app.run(host='0.0.0.0', port=port)
